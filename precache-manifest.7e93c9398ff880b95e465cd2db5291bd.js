592dc5211d03059f0b6553b0197d842e8d9e76bc
Anton Cherkasov <anton.cherkasov@gmail.com>
2019-03-15 10:33:36 +0300

    Updates

---
+++ precache-manifest.7e93c9398ff880b95e465cd2db5291bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "78b592f9426f98d42f6a",
+    "url": "/contactmanager/static/js/main.78b592f9.chunk.js"
+  },
+  {
+    "revision": "d9d4f53b0c9b38eb181a",
+    "url": "/contactmanager/static/js/2.d9d4f53b.chunk.js"
+  },
+  {
+    "revision": "78b592f9426f98d42f6a",
+    "url": "/contactmanager/static/css/main.06e14110.chunk.css"
+  },
+  {
+    "revision": "d9d4f53b0c9b38eb181a",
+    "url": "/contactmanager/static/css/2.77ccc49d.chunk.css"
+  },
+  {
+    "revision": "a2f4c88b3e7b84518174302b2a7258c8",
+    "url": "/contactmanager/index.html"
+  }
+];
